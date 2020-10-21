12d041a352b6e41d414117667798a9e62847d0fd
mahaMohamed <maha.fagal@gmail.com>
2020-10-21 11:35:39 +0200

    Added Hovering Effect

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
+
+
+
+
 const sketchingArea = document.querySelector('#sketching-area');
+
+
+/* Prepping the skteching area for drawing a grid*/ 
 sketchingArea.style.gridTemplateColumns = 'repeat(16, 1fr)';
 sketchingArea.style.gridTemplateRows = 'repeat (16,1fr)';
 
-// const squareBlocks = document.querySelector(".squares")
-
-const div = document.createElement('div');                     
+/*Dynamically creating the squares of the grid*/
+const div = document.createElement('div');
 div.classList.add('squares');
-div.style.borderRight = "0.1px solid black"; 
-div.style.borderBottom = "0.1px solid black"; 
+div.style.borderRight = "0.1px solid black";
+div.style.borderBottom = "0.1px solid black";
 
-
-
-
-for (let i=0; i<16*16; i++){
+for (let i = 0; i < 16 * 16; i++) {
     sketchingArea.appendChild(div.cloneNode());
-    console.log(i);
 }
 
+/*Setting up the hover effect*/
+const squareBlocks = document.querySelectorAll(".squares"); 
+
+for (let i=0; i<squareBlocks.length; i++){
+    squareBlocks[i].addEventListener("mouseenter", colorSquares);
+}
+
+function colorSquares(e){
+    e.target.style.backgroundColor = "black";
+    console.log(e.target);
+    console.log("here");
+}
