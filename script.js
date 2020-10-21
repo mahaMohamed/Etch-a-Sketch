1c95a4dd0bab55cc49d9cd8e5dd82150e18848fc
mahaMohamed <maha.fagal@gmail.com>
2020-10-21 17:59:51 +0200

    Added Clear Grid Button

--- script.js
+++ script.js
@@ -3,31 +3,70 @@
 
 
 const sketchingArea = document.querySelector('#sketching-area');
-
-
-/* Prepping the skteching area for drawing a grid*/ 
-sketchingArea.style.gridTemplateColumns = 'repeat(16, 1fr)';
-sketchingArea.style.gridTemplateRows = 'repeat (16,1fr)';
+const clearGridButton = document.querySelector('#clear-grid');
 
 /*Dynamically creating the squares of the grid*/
-const div = document.createElement('div');
-div.classList.add('squares');
-div.style.borderRight = "0.1px solid black";
-div.style.borderBottom = "0.1px solid black";
+function createGrid(gridSize) {
+    /* Prepping the skteching area for drawing a grid*/
+    sketchingArea.style.gridTemplateColumns = 'repeat(' + gridSize + ', 1fr)';
+    sketchingArea.style.gridTemplateRows = 'repeat(' + gridSize + ', 1fr)';
 
-for (let i = 0; i < 16 * 16; i++) {
-    sketchingArea.appendChild(div.cloneNode());
+    const div = document.createElement('div');
+    div.classList.add('squares');
+    div.style.borderRight = "0.1px solid black";
+    div.style.borderBottom = "0.1px solid black";
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        sketchingArea.appendChild(div.cloneNode());
+    }
+
+    addHoverEffect();
 }
 
 /*Setting up the hover effect*/
-const squareBlocks = document.querySelectorAll(".squares"); 
+function addHoverEffect() {
+    const squareBlocks = document.querySelectorAll(".squares");
 
-for (let i=0; i<squareBlocks.length; i++){
-    squareBlocks[i].addEventListener("mouseenter", colorSquares);
+    for (let i = 0; i < squareBlocks.length; i++) {
+        squareBlocks[i].addEventListener("mouseenter", colorSquares);
+    }
+
+    function colorSquares(e) {
+        e.target.style.backgroundColor = "black";
+        console.log(e.target);
+        console.log("here");
+    }
 }
 
-function colorSquares(e){
-    e.target.style.backgroundColor = "black";
-    console.log(e.target);
-    console.log("here");
+createGrid(16);
+
+
+
+/*Clearing the grid and prompting the user to add custom size of the grid*/
+clearGridButton.addEventListener('click', clearGrid);
+
+function clearGrid() {
+
+    const squareBlocks = document.querySelectorAll(".squares");
+    console.log("clear grid")
+    for (let i = 0; i < squareBlocks.length; i++) {
+        squareBlocks[i].style.backgroundColor = "white";
+    }
+
+
+    let customGridSize = 0; 
+
+    do{
+         customGridSize = window.prompt("Enter new grid size, max 100");
+    } while (customGridSize > 100);
+
+    for (let i = 0; i < squareBlocks.length; i++) {
+        sketchingArea.removeChild(squareBlocks[i]);
+    }
+    createGrid(customGridSize);
+
+
 }
+
+
+
